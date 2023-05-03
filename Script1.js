91a35730468632b9ea72c06c7036dad32a06824b
Nyasha Kunze <Nyasha Kunze>
2023-05-03 12:54:41 +0100

    Layout of  individual recipe pages

---
+++ Script1.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+//code to search thorugh html files for recipes
+function search_recipe(){
+	let input = document.getElementById('searchbar').valueinput=input.toLowerCase();
+	let x = document.getElementByClassName('recipes');
+
+	for (i = 0; i < x.lenght; i++){
+		if (!x[i]).innerHTML.toLowerCase().includes(input)){
+			x[i].style.display="none";
+		}
+		else{
+			x[i].style.display="list-item";
+		}
+	}
+}
